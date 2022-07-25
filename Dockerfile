59d214f3f6092b87b0d9c68bdee3f77f81102a97
rsbyrne <rohan.byrne@gmail.com>
2022-07-25 01:33:25 +0000

    Quick push

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,9 @@ MAINTAINER https://github.com/rsbyrne/
 
 USER root
 
+RUN pip3 install -U --no-cache-dir \
+  html5lib \
+  lxml
+
 WORKDIR $MOUNTDIR
 USER $MASTERUSER
--- lb.sh
+++ lb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-currentDir=$PWD
-cd "$(dirname "$0")"
+#currentDir=$PWD
+#cd "$(dirname "$0")"
 MOUNTFROM=$PWD
 MOUNTTO='/home/morpheus/workspace/mount'
 IMAGE='rsbyrne/risk-portal'
@@ -8,4 +8,4 @@ SOCK='/var/run/docker.sock'
 docker run -v $MOUNTFROM:$MOUNTTO -v $SOCK:$SOCK --shm-size 2g -p 8888:8888 $IMAGE \
   jupyter lab --no-browser --allow-root --port=8888 --ip='0.0.0.0'
 docker container prune -f
-cd $currentDir
+#cd $currentDir
