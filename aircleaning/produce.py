be8690732c6d12a321b3def19d6b92d691f942f3
rsbyrne <rohan.byrne@gmail.com>
2022-07-25 05:50:48 +0000

    Quick push

--- aircleaning/produce.py
+++ aircleaning/produce.py
@@ -20,6 +20,7 @@ def cost_analysis(data, volume, quality, path=productsdir, name='default'):
 
     data = analyse.cost_analysis(data, volume, quality)
     data = data.sort_values('nominal')
+    data = data.loc[data['nunits'] < 6]
     data['fullname'] = tuple(map(
         ''.join, zip(
             map(' '.join, data.index),
@@ -34,18 +35,27 @@ def cost_analysis(data, volume, quality, path=productsdir, name='default'):
 
     y_pos = np.arange(len(data))
 
-    ax1.barh(y_pos, data['nominal'])
+    bars = ax1.barh(y_pos, data['nominal'])
     ax1.barh(y_pos, data['upfront'])
     ax1.set_yticks(y_pos, labels=data['fullname'])
     ax1.invert_yaxis()  # labels read top-to-bottom
     ax1.set_xlabel('Dollars ($)')
     ax1.set_title('Cost (upfront + 6-year running)')
+    ax1.bar_label(bars, label_type='edge', padding=8, fmt='$%d')
+    # ax1.spines['top'].set_visible(False)
+    ax1.spines['right'].set_visible(False)
+    ax1.spines['left'].set_visible(False)
 
-    ax2.barh(y_pos, data['noise'])
+    bars = ax2.barh(y_pos, data['noise'], color='green')
     ax2.set_yticks(y_pos, labels=[])
     ax2.invert_yaxis()  # labels read top-to-bottom
     ax2.set_xlabel('Decibels (dB)')
     ax2.set_title('Running noise')
+    ax2.set_xlim(20)
+    ax2.bar_label(bars, label_type='edge', padding=8, fmt='%d dB')
+    # ax2.spines['top'].set_visible(False)
+    ax2.spines['right'].set_visible(False)
+    ax2.spines['left'].set_visible(False)
 
     # title = f"Air cleaner choices: a {volume}-sized room with {quality} air quality"
     # fig.suptitle(title, fontproperties=dict(weight='heavy'))
