c49bc7328f57c69abb661e59c7e45f882ba5ace4
rsbyrne <rohan.byrne@gmail.com>
2022-07-25 06:03:37 +0000

    Quick push

--- aircleaning/produce.py
+++ aircleaning/produce.py
@@ -30,32 +30,42 @@ def cost_analysis(data, volume, quality, path=productsdir, name='default'):
 
     plt.rcdefaults()
     fig, (ax1, ax2) = plt.subplots(ncols=2)
-    fig.set_size_inches(8, 0.25 * len(data))
+    fig.set_size_inches(8, 0.3 * len(data))
     fig.set_tight_layout(True)
 
     y_pos = np.arange(len(data))
 
-    bars = ax1.barh(y_pos, data['nominal'])
-    ax1.barh(y_pos, data['upfront'])
+    outerbars = ax1.barh(y_pos, data['nominal'])
+    innerbars = ax1.barh(y_pos, data['upfront'])
     ax1.set_yticks(y_pos, labels=data['fullname'])
     ax1.invert_yaxis()  # labels read top-to-bottom
     ax1.set_xlabel('Dollars ($)')
-    ax1.set_title('Cost (upfront + 6-year running)')
-    ax1.bar_label(bars, label_type='edge', padding=8, fmt='$%d')
-    # ax1.spines['top'].set_visible(False)
+    ax1.set_title('Cost')
+    ax1.bar_label(outerbars, label_type='edge', padding=8, fmt='$%d')
+    ax1.spines['top'].set_visible(False)
     ax1.spines['right'].set_visible(False)
     ax1.spines['left'].set_visible(False)
+    ax1.tick_params(axis='y', which='both', left=False)
+    ax1.legend(
+        [innerbars, outerbars], ['$ Upfront', '$ 6-year running'],
+        ncol=2,
+        )
 
     bars = ax2.barh(y_pos, data['noise'], color='green')
     ax2.set_yticks(y_pos, labels=[])
     ax2.invert_yaxis()  # labels read top-to-bottom
     ax2.set_xlabel('Decibels (dB)')
-    ax2.set_title('Running noise')
+    ax2.set_title('Noise')
     ax2.set_xlim(20)
     ax2.bar_label(bars, label_type='edge', padding=8, fmt='%d dB')
-    # ax2.spines['top'].set_visible(False)
+    ax2.spines['top'].set_visible(False)
     ax2.spines['right'].set_visible(False)
     ax2.spines['left'].set_visible(False)
+    ax2.tick_params(axis='y', which='both', left=False)
+    ax2.legend(
+        [bars,], ['Sound rating in decibels'],
+        ncol=2,
+        )
 
     # title = f"Air cleaner choices: a {volume}-sized room with {quality} air quality"
     # fig.suptitle(title, fontproperties=dict(weight='heavy'))
